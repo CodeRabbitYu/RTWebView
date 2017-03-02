9416a3c2b26a77aa8fda12e9a8f45e32604a3939
Rabbit <Rabbit@RabbitdeMacBook-Pro.local>
2017-03-02 23:03:28 +0800

    Initial Commit

---
+++ RTWebView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RTWebView
+//
+//  Created by Rabbit on 2017/3/2.
+//  Copyright © 2017年 Rabbit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
